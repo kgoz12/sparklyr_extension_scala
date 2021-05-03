14eef30731a1802ab2bf6da8885d0e831993786b
Katie Goznikar <kmgoznikar0084@gmail.com>
2021-05-03 16:54:58 -0400

    jar and namespace changes

--- R/dependencies.R
+++ R/dependencies.R
@@ -6,7 +6,6 @@ spark_dependencies <- function(spark_version, scala_version, ...) {
     jars = c(
       system.file(
         sprintf("java/sparklyrExtensionScala-3.0-2.12.jar", spark_version, scala_version),
-        # sprintf("java/sparklyrExtensionScala-3.0-2.12.jar", "3.0.1", "2.12"),
         package = "sparklyrExtensionScala"
       )
     ),
